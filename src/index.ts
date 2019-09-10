1d9c5095f8685e2f94ae3a384b0f608fdfabfcc1
maxim <xydenswork@gmail.com>
2019-09-10 15:42:14 +0500

    feat: export holdOn function

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export {sleep} from "./sleep"
 export {DelayedCall, DelayedCallOptions, DelayedCallStatus} from "./delayed-call"
 export {Schedule} from "./schedule"
+export {holdOn} from "./hold-on"
